ebf4b86bd198a0008de11ba6e3118fe2f0f6cc30
Nover <onverx@gmail.com>
2022-10-02 21:49:10 +0500

    Добавил кнопки

--- ModelView/model.py
+++ ModelView/model.py
@@ -1,5 +1,6 @@
-from PyQt5.QtCore import QAbstractTableModel, Qt, QAbstractListModel
-from PyQt5.QtGui import QPainter
+from PyQt5.QtCore import QAbstractTableModel, Qt, QAbstractListModel, QModelIndex
+from PyQt5.QtGui import QPainter, QBrush
+
 
 class Rectangle():
     def __init__(self, x, y, width, height):
@@ -34,14 +35,58 @@ class CustomModel(QAbstractListModel):
         return len(self._rects)
 
     def columnCount(self, parent=None):
-        return 4
+        return len(self._rects)
 
     def data(self, index, role=Qt.DisplayRole):
         row = index.row()
         col = index.column()
 
         # generate a log message when this method gets called
-        print(f"row {row}, col{col}, role {role}")
 
         if role == Qt.DisplayRole:
             return f"{self._rects[row].Area()}"
+        elif role == Qt.BackgroundRole:
+            if row % 4 == 0:  # change background only for cell(1,2)
+                return QBrush(Qt.cyan)
+
+    def setData(self, index, value, role=Qt.EditRole):
+        """Changes an item in the string list, but only if the following conditions
+           are met:
+
+           # The index supplied is valid.
+           # The index corresponds to an item to be shown in a view.
+           # The role associated with editing text is specified.
+
+           The dataChanged() signal is emitted if the item is changed."""
+
+        if index.isValid() and role == Qt.EditRole:
+            self._rects[index.row()] = value
+            self.dataChanged.emit(index, index, {role})
+            return True
+        return False
+
+    def headerData(self, section, orientation, role=Qt.DisplayRole):
+        """Returns the appropriate header string depending on the orientation of
+           the header and the section. If anything other than the display role is
+           requested, we return an invalid variant."""
+        if role != Qt.DisplayRole:
+            return None
+        if orientation == Qt.Horizontal:
+            return f"Column {section}"
+        return f"Row {section}"
+
+    def insertRows(self, position, rows, parent):
+        """Inserts a number of rows into the model at the specified position."""
+        self.beginInsertRows(QModelIndex(), position, position + rows - 1)
+        for row in range(rows):
+            self._rects.insert(position, "")
+        self.endInsertRows()
+        return True
+
+    def removeRows(self, position, rows, parent):
+        """Removes a number of rows from the model at the specified position."""
+        self.beginRemoveRows(QModelIndex(), position, position + rows - 1)
+        for row in range(rows):
+            del self._rects[position]
+        self.endRemoveRows()
+        return True
--- ModelView/view.py
+++ ModelView/view.py
@@ -5,40 +5,53 @@ from model import  CustomModel
 from PyQt5 import Qt
 from PyQt5.QtCore import Qt, QModelIndex
 from PyQt5.QtWidgets import QApplication, \
-    QMainWindow, QLabel, QComboBox, QGridLayout, QPushButton, QWidget, QTableView, QListView, QTreeView
+    QMainWindow, QLabel, QComboBox, QGridLayout, QPushButton, QWidget, QTableView, QListView, QTreeView, QLineEdit, \
+    QSpinBox, QListWidget, QListWidgetItem
 from PyQt5.QtGui import QPalette, QColor
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
+        self.myrects = []
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle("Lab 4")
-        self.setGeometry(300, 300, 350, 100)
-        self.resize(600, 600)
-        myrects = []
+        self.setGeometry(100, 50, 300, 300)
+        self.resize(800, 800)
+        #listWidget = QListWidget(self)
         for i in range(16):
-            myrects.append(Rectangle(i, i, i * 16, i * 16))
-        model = CustomModel(myrects)
-
+            self.myrects.append(Rectangle(i, i, i * 16, i * 16))
+        model = CustomModel(self.myrects)
         tableView = QTableView()
         tableView.setModel(model)
-        tableView.adjustSize()
-
+        
         listView = QListView()
         listView.setModel(model)
 
         treeView = QTreeView()
         treeView.setModel(model)
-
+        layout2 = QGridLayout()
         layout = QGridLayout()
         views = QComboBox()
-        views.addItems(["One", "Two", "Three"])
         layout.addWidget(tableView, 0, 0)
         layout.addWidget(listView, 0, 1)
         layout.addWidget(treeView, 0, 2)
-
+        layout2.addLayout(layout, 0, 0)
+        btn = QPushButton("Add Rectangle")
+        btn.clicked.connect(self.addRectangle)
+        layout2.addWidget(btn, 1, 1)
+        self.width = QSpinBox()
+        self.height = QSpinBox()
+        layout2.addWidget(self.width, 2, 1)
+        layout2.addWidget(self.height, 3, 1)
+        layout2.addWidget(QLabel("Add Rectangle: "), 1, 0)
+        layout2.addWidget(QLabel("Width: "), 2, 0)
+        layout2.addWidget(QLabel("Height: "), 3, 0)
         widget = QWidget()
-        widget.setLayout(layout)
+        widget.setLayout(layout2)
         self.setCentralWidget(widget)
+    def addRectangle(self):
+        print("a")
+        QListWidgetItem(Rectangle(1, 1, self.width.value(), self.height.value()), self.listWidget)
+        print("b")
